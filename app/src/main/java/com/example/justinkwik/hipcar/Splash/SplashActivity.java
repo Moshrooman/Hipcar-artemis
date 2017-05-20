0700a8b4072a26c2c83652be51c70e698c473b2c
Moshrooman <justinkwik@blackgarlic.id>
2017-05-20 12:14:20 -0500

    Splash screen using Lottie

--- app/src/main/java/com/example/justinkwik/hipcar/Splash/SplashActivity.java
+++ app/src/main/java/com/example/justinkwik/hipcar/Splash/SplashActivity.java
@@ -1,23 +1,35 @@
 package com.example.justinkwik.hipcar.Splash;
 
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.justinkwik.hipcar.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SplashActivity extends AppCompatActivity {
 
     private ImageView hipCarLogoImageView;
-    private LottieAnimationView testLottie; //TODO: Delete and make a hipcar animation, one going off the other, make color of letter
-    //TODO: RED
+    private LottieAnimationView aLotte;
+    private LottieAnimationView rLotte;
+    private LottieAnimationView tLotte;
+    private LottieAnimationView eLotte;
+    private LottieAnimationView mLotte;
+    private LottieAnimationView iLotte;
+    private LottieAnimationView sLotte;
     private AlphaAnimation fadeIn;
 
     @Override
@@ -25,14 +37,19 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        hipCarLogoImageView = (ImageView) findViewById(R.id.hipCarLogoImageView); //TODO: delete
-        testLottie = (LottieAnimationView) findViewById(R.id.testLottie);
+        hipCarLogoImageView = (ImageView) findViewById(R.id.hipCarLogoImageView);
+        aLotte = (LottieAnimationView) findViewById(R.id.aLotte);
+        rLotte = (LottieAnimationView) findViewById(R.id.rLotte);
+        tLotte = (LottieAnimationView) findViewById(R.id.tLotte);
+        eLotte = (LottieAnimationView) findViewById(R.id.eLotte);
+        mLotte = (LottieAnimationView) findViewById(R.id.mLotte);
+        iLotte = (LottieAnimationView) findViewById(R.id.iLotte);
+        sLotte = (LottieAnimationView) findViewById(R.id.sLotte);
+
         fadeIn = new AlphaAnimation(0, 1);
         fadeIn.setDuration(2000);
 
-        hipCarLogoImageView.startAnimation(fadeIn); //TODO: Delete
-        testLottie.playAnimation();
-
+        hipCarLogoImageView.startAnimation(fadeIn);
 
     }
 
