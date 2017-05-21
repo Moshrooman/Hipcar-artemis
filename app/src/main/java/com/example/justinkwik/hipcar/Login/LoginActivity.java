e282a4a25c461c1097d4273920ad365e2a186480
Moshrooman <justinkwik@blackgarlic.id>
2017-05-21 01:33:57 -0500

    Working on login screen

--- app/src/main/java/com/example/justinkwik/hipcar/Login/LoginActivity.java
+++ app/src/main/java/com/example/justinkwik/hipcar/Login/LoginActivity.java
@@ -1,21 +1,60 @@
 package com.example.justinkwik.hipcar.Login;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.justinkwik.hipcar.R;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //TODO import the volley string request.
+
+        usernameEditText = (EditText) findViewById(R.id.userNameEditText);
+        passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        loginButton = (Button) findViewById(R.id.loginButton);
+
+        loginButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                    loginButton.setBackgroundResource(R.drawable.loginbuttonpressed);
+
+                } else {
+
+                    loginButton.setBackgroundResource(R.drawable.loginbutton);
+
+                }
+
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 
 }
